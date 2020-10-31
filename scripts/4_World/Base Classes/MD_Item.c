2e1b77f5126c6120990fdf0fecb5fca8052b7bdd
helkhiana <helkhiana@gmail.com>
2020-10-31 13:20:50 +0000

    1.10 preparation

--- scripts/4_World/Base Classes/MD_Item.c
+++ scripts/4_World/Base Classes/MD_Item.c
@@ -12,16 +12,21 @@ class MD_Item: ItemBase
 };
 
 class MD_Item_Kit: ItemBase
-{		
-	override void OnPlacementComplete( Man player )
+{	
+	#ifdef DAYZ_1_09	
+	override void OnPlacementComplete(Man player)
 	{
-		super.OnPlacementComplete( player );		
+		super.OnPlacementComplete(player);
+		PlayerBase player_base = PlayerBase.Cast(player);
+		vector position = player_base.GetLocalProjectionPosition();
+		vector orientation = player_base.GetLocalProjectionOrientation();
+	#else
+	override void OnPlacementComplete(Man player, vector position = "0 0 0", vector orientation = "0 0 0")
+	{
+		super.OnPlacementComplete(player, position, orientation);
+	#endif
 		if ( GetGame().IsServer() )
-		{
-			PlayerBase player_base = PlayerBase.Cast( player );
-			vector position = player_base.GetLocalProjectionPosition();
-			vector orientation = player_base.GetLocalProjectionOrientation();
-				
+		{				
 			EntityAI kitItem = EntityAI.Cast(GetGame().CreateObject(Get_MDItemName(), position, false ));
 			kitItem.SetPosition(position);
 			kitItem.SetOrientation(orientation);			
