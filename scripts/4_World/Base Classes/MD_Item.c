000875303b02543407e8da186ebd84645f568693
helkhiana <helkhiana@gmail.com>
2021-10-18 17:27:48 +0100

    Fix for placeentity

--- scripts/4_World/Base Classes/MD_Item.c
+++ scripts/4_World/Base Classes/MD_Item.c
@@ -13,18 +13,9 @@ class MD_Item: ItemBase
 
 class MD_Item_Kit: ItemBase
 {	
-	#ifdef DAYZ_1_09	
-	override void OnPlacementComplete(Man player)
-	{
-		super.OnPlacementComplete(player);
-		PlayerBase player_base = PlayerBase.Cast(player);
-		vector position = player_base.GetLocalProjectionPosition();
-		vector orientation = player_base.GetLocalProjectionOrientation();
-	#else
 	override void OnPlacementComplete(Man player, vector position = "0 0 0", vector orientation = "0 0 0")
 	{
 		super.OnPlacementComplete(player, position, orientation);
-	#endif
 		if ( GetGame().IsServer() )
 		{				
 			EntityAI kitItem = EntityAI.Cast(GetGame().CreateObject(Get_MDItemName(), position, false ));
@@ -33,6 +24,11 @@ class MD_Item_Kit: ItemBase
 		}
 		
 		SetIsPlaceSound( true );
+	}
+	
+	override bool IsBasebuildingKit()
+	{
+		return true;
 	}
 	
 	override bool IsDeployable()
--- scripts/4_World/Modded Classes/Hologram.c
+++ scripts/4_World/Modded Classes/Hologram.c
@@ -14,14 +14,6 @@ modded class Hologram
 		}
 	}
 
-	EntityAI PlaceEntity(EntityAI entity_for_placing)
-	{
-		if (entity_for_placing.IsInherited(MD_Item_Kit) || entity_for_placing.IsInherited(FieldShovel))
-			return entity_for_placing;
-
-		return super.PlaceEntity(entity_for_placing);
-	}
-
 	override string GetProjectionName(ItemBase item)
 	{
 		if (item.CanMakeMD_Grave())
@@ -57,30 +49,11 @@ modded class Hologram
 	{
 		if (m_Parent.IsInherited(MD_Item_Kit) || m_Parent.IsInherited(MD_StorageItem) && IsFloating())
 		{
-			vector mdItemPos = SetOnGroundOld(position) + m_Parent.Get_MDItemPos();
+			vector mdItemPos = SetOnGround(position) + m_Parent.Get_MDItemPos();
 			m_Projection.SetPosition(mdItemPos);
 			return;
 		}
 
 		super.SetProjectionPosition(position);
-	}	
-
-	vector SetOnGroundOld( vector position )
-	{
-		vector from = position;
-		vector ground;
-		vector player_to_projection_vector;
-		float projection_diameter = GetProjectionDiameter();
-			
-		ground = Vector(0, - Math.Max( projection_diameter, SMALL_PROJECTION_GROUND ), 0);	
-		vector to = from + ground;
-		vector contact_pos;
-		int contact_component;
-		
-		DayZPhysics.RaycastRV( from, to, contact_pos, m_ContactDir, contact_component, NULL, NULL, m_Projection, false, false );
-
-		HideWhenClose( contact_pos );
-
-		return contact_pos;
 	}
 };
